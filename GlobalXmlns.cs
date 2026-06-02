c23f03db1e7d7bb1e12d6d44cbe5e5e78046d178
fve <fve@URB6-LAB1-50339>
2026-06-02 19:51:36 -0300

    Adicionar arquivos de projeto.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "App_FGNC")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "App_FGNC.Pages")]
