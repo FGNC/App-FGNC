bcc5d11601a4970208786469f0d6be68b8662087
FGNC <120611000+FGNC@users.noreply.github.com>
2026-06-09 16:54:04 -0300

    Add User model with properties Id, Usuario, and Senha

--- Models/User.cs
+++ Models/User.cs
@@ -1,1 +1,9 @@
-
+namespace App_FGNC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
