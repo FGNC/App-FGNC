d1c8cce24ad6c3e1acf21a0929465bc2ee52e681
FGNC <120611000+FGNC@users.noreply.github.com>
2026-06-09 16:52:01 -0300

    Implement login functionality in LoginPage

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -1,1 +1,27 @@
+using App_FGNC.Controllers;
 
+namespace App_FGNC.Views;
+
+public partial class LoginPage : ContentPage
+{
+    AuthController controller = new AuthController();
+
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        bool sucesso = controller.Login(txtUsuario.Text, txtSenha.Text);
+
+        if (sucesso)
+        {
+            Application.Current.MainPage = new AppShell();
+        }
+        else
+        {
+            lblMensagem.Text = "Login inválido!";
+        }
+    }
+}
