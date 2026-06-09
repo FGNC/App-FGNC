f315d71060d4d27b107c67e139fa70ac24d1f0f7
FGNC <120611000+FGNC@users.noreply.github.com>
2026-06-09 16:50:23 -0300

    Add user greeting and logout functionality to HomePage

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,1 +1,23 @@
+using App_FGNC.Controllers;
+using Microsoft.Maui.Storage;
 
+namespace App_FGNC.Views;
+
+public partial class HomePage : ContentPage
+{
+    AuthController controller = new AuthController();
+
+    public HomePage()
+    {
+        InitializeComponent();
+
+        string user = Preferences.Default.Get("usuario", "");
+        lblUser.Text = $"Bem-vindo {user}";
+    }
+
+    private void OnLogoutClicked(object sender, EventArgs e)
+    {
+        controller.Logout();
+        Application.Current.MainPage = new LoginPage();
+    }
+}
